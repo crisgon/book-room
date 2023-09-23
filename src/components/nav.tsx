e0df995797276625bc5d14ff997c22a4e5b0251d
Cristiano Goncalves <cristianogoncalves@pop-os.localdomain>
2023-09-22 23:29:48 -0300

    feat: improve styles

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -12,32 +12,49 @@ import {
 import { Button } from "@/components/ui/button";
 import BookingForm from "@/components/booking-form";
 import { useState } from "react";
+import { Bookmark, Home } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 export function Nav() {
+  const path = usePathname();
   const [open, setOpen] = useState(false);
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <nav className="w-[1000px] flex items-center py-8 space-x-4 m-auto">
+      <div className="m-auto  w-[300px] py-10 flex flex-col items-center gap-4">
+        <Home size={48} />
+        <h1 className="text-xl font-bold">Reserva de auditórios</h1>{" "}
+      </div>
+      <nav className="w-[500px] flex items-center py-8 space-x-4 m-auto">
         <DialogTrigger asChild>
-          <Button variant="default">Criar reserva</Button>
+          <Button variant="default" className="bg-blue-500 lex gap-2">
+            <Bookmark size={16} />
+            Reservar
+          </Button>
         </DialogTrigger>
         <Link
           href="/"
-          className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+          className={cn(
+            "text-sm font-medium text-muted-foreground transition-colors hover:text-primary",
+            path === "/" ? "underline text-white font-bold" : ""
+          )}
         >
           Calendário de reservas
         </Link>
         <Link
           href="/reservas"
-          className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+          className={cn(
+            "text-sm font-medium text-muted-foreground transition-colors hover:text-primary",
+            path === "/reservas" ? "underline text-white font-bold" : ""
+          )}
         >
-          Lista de reservas
+          Todas as reservas
         </Link>
       </nav>
-
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Reservar Sala</DialogTitle>
+          <DialogTitle>Reservar Sala </DialogTitle>
           <DialogDescription>Faça a reserva de uma sala</DialogDescription>
         </DialogHeader>
         <BookingForm onCloseModal={() => setOpen(false)} />
